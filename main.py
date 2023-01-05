ab7e00c8c75ec324930484fea3bee5840b818bdd
Roman <roman.polnik@esas.autocont.sk>
2023-01-05 09:49:25 +0100

    guiInterface

--- main.py
+++ main.py
@@ -8,57 +8,60 @@ obj2 = App.EmailSender()
 obj3 = App.SmsSender()
 obj4 = App.SharePointListUpload()
 
-global number
-global number1
 
 list_name = obj.get_all_quotes()
-number = random.randint(0, len(list_name))
+number = random.randint(1, len(list_name)-1)
+number1 = number
 
-lst=[]
-lst.append(number)
 
 root = tk.Tk()
 root.title("Motivation quotes")
-root.geometry("400x600")
+root.geometry("400x650")
 
 
 def next_button_click():
-    number1 = random.randint(0, len(list_name))
-    if number1==0:
-        number1=1
-    lst.append(number1)
+    global number1
+    number1 += 1
+    if number1 == len(list_name)-1:
+        number1 = 1
     label.config(text=list_name[number1].text)
 
+
 def previous_button_click():
-    if len(lst)>1:
-        label.config(text=list_name[lst[len(lst)-2]].text)
+    global number1
+    number1 -= 1
+    if number1 >= 1:
+        label.config(text=list_name[number1].text)
+    else:
+        number1 = 1
+
+
 
 def send_quote_by_sms():
-    obj3.send_sms(list_name[number].text, '+421907474723')
+    obj3.send_sms(list_name[number1].text, '')
 
 
 def send_quote_by_email():
-    obj2.send_email(list_name[number].text)
+    obj2.send_email(list_name[number1].text)
+
 
 def send_quote_to_sharepoint():
-    obj4.upload_data("Motivation", list_name[number].text)
+    obj4.upload_data("Motivation", list_name[number1].text)
 
 
+button1 = tk.Button(root, text="Next Quote", height=3, width=20, command=next_button_click, bg="green")
+button2 = tk.Button(root, text="Previous Quote", height=3, width=20, command=previous_button_click, bg="green")
+button3 = tk.Button(root, text="Send Quote by SMS", height=3, width=20, command=send_quote_by_sms, bg="green")
+button4 = tk.Button(root, text="Send Quote by Email", height=3, width=20, command=send_quote_by_email, bg="green")
+button5 = tk.Button(root, text="Send Quote to Sharepoint", height=3, width=20, command=send_quote_to_sharepoint, bg="green")
+button1.place(x=120,y=175)
+button2.place(x=120, y=275)
+button3.place(x=120, y=375)
+button4.place(x=120, y=475)
+button5.place(x=120, y=575)
+label = tk.Label(root, text=list_name[number].text, wraplength=350, anchor="n")
 
-button1 = tk.Button(root, text="Next Quote", height=3, width=20, command=next_button_click)
-button2 = tk.Button(root, text="Previous Quote", height=3, width=20, command=previous_button_click)
-button3 = tk.Button(root, text="Send Quote by SMS", height=3, width=20, command=send_quote_by_sms)
-button4 = tk.Button(root, text="Send Quote by Email", height=3, width=20, command=send_quote_by_email)
-button5 = tk.Button(root, text="Send Quote to Sharepoint", height=3, width=20, command=send_quote_to_sharepoint)
-button1.place(x=120,y=100)
-button2.place(x=120, y=200)
-button3.place(x=120, y=300)
-button4.place(x=120, y=400)
-button5.place(x=120, y=500)
-label = tk.Label(root, text=list_name[number].text)
-label.place(x=0, y=10)
 label.pack()
 
-#button1.pack()
-
 root.mainloop()
+
